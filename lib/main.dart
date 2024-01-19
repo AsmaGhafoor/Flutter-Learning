53a2bc7789cba10d5086336206b2c4ffa001e557
asmaghafoor <asmaghafoorofficial@gmail.com>
2024-01-19 02:46:22 -0800

    test 3

--- lib/main.dart
+++ lib/main.dart
@@ -31,5 +31,54 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello Flutter"),
+        ),
+        //tested a new comment
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                            height: 200,
+                            width: 100,
+                            color: Colors.orange,
+                            margin: const EdgeInsets.only(right: 11)),
+                        Container(
+                            height: 200,
+                            width: 150,
+                            color: Colors.grey,
+                            margin: const EdgeInsets.only(right: 11)),
+                        Container(
+                            height: 200,
+                            width: 200,
+                            color: Colors.pink,
+                            margin: const EdgeInsets.only(right: 11)),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.blue,
+                    margin: const EdgeInsets.only(bottom: 11)),
+                Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.lightGreen,
+                    margin: const EdgeInsets.only(bottom: 11))
+              ],
+            ),
+          ),
+        ));
+  }
 }
