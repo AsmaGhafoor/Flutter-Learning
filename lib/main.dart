7783e9e53356c6d264b88372f9b805a599af9b97
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2024-01-23 14:54:01 +0500

    Circle Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,20 @@ class MyApp extends StatelessWidget {
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var arrNames=['HTML','CSS','JavaScript','React'];
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
       ),
       
-      body: ListView.separated(itemBuilder: (context,index){
-        return ListTile(
-          leading: Text('${index+1}'),
-          title:Text(arrNames[index]),
-          subtitle: const Text('Number'),
-          trailing:const Icon(Icons.add),
-        );
-      }, 
-      itemCount: arrNames.length, 
-      separatorBuilder: (context,index){
-      return const Divider(height: 20,thickness:1,);
-      },
+      body: const Center(
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/image1.jpg'),
+            backgroundColor: Colors.green,
+          ),
+        ),
       )
     );
   }
