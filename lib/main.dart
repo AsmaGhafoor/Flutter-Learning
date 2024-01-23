716ae0662fe6e6ac583c044c3f60fd9c57c82ee2
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2024-01-23 15:27:16 +0500

    Circle Avator with Text

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,34 @@ class DashboardScreen extends StatelessWidget {
         title: const Text("Dashboard"),
       ),
       
-      body: const Center(
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: CircleAvatar(
-            backgroundImage: AssetImage('assets/images/image1.jpg'),
-            backgroundColor: Colors.green,
+      body: 
+      // const Center(
+      //   child: SizedBox(
+      //     width: 100,
+      //     height: 100,
+      //     child: CircleAvatar(
+      //       backgroundImage: AssetImage('assets/images/image1.jpg'),
+      //       backgroundColor: Colors.green,
+      //     ),
+      //   ),
+      // )
+      Center(
+        child: CircleAvatar(
+          // ignore: sized_box_for_whitespace
+          backgroundColor: Colors.green,
+          maxRadius: 60,
+          // ignore: sized_box_for_whitespace
+          child: Container(
+            width:70,
+            height:70,
+            child:Column(
+              // ignore: sized_box_for_whitespace
+              children: [Container(
+                width:40,
+                height:40,
+                child:Image.asset('assets/images/image1.jpg')
+              ),Text('Name', style:TextStyle(color:Colors.red[200]))],
+            ),
           ),
         ),
       )
